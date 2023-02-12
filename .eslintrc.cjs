38835452ea53339d598a8565a53c3aaef8f70c48
Kszalank <szalankiewiczkrzysiek@gmail.com>
2023-02-12 10:55:43 +0100

    setup in progress

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "react-hooks", "prettier"],
+  rules: {
+    "prettier/prettier": ["error"],
+    camelcase: "error",
+    "react/react-in-jsx-scope": "off",
+    "no-duplicate-imports": "error",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+  },
+};
