f626b01e84042dfc336ccb285571f7f8f0db0ba0
Kszalank <szalankiewiczkrzysiek@gmail.com>
2023-02-18 10:36:44 +0100

    headers added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.scss";
+import FirstHeader from "./components/firstHeader";
+import SecondHeaderV1 from "./components/secondHeaderVariant1";
+import SecondHeaderV2 from "./components/secondHeaderVariant2";
 
 function App() {
-  return <div className="App">Kutas</div>;
+  return (
+    <div className="App">
+      <FirstHeader />
+      <SecondHeaderV1 />
+      <SecondHeaderV2 />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/secondHeaderVariant2.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SecondHeaderV2() {
+  return (
+    <div>
+      <h2 className="header-2">Convert values</h2>
+    </div>
+  );
+}
+
+export default SecondHeaderV2;
