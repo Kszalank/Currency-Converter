d4896133f44d0e8bf558f88e40c7e85399d675d6
Kszalank <szalankiewiczkrzysiek@gmail.com>
2023-02-20 18:32:16 +0100

    heading chagned

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 
 function Heading({ variant, children }) {
-  if (variant === "title") {
-    return <h1 className="heading-1">{children}</h1>;
-  }
-  if (variant === "subtitle") {
-    return <h2 className="heading-2">{children}</h2>;
-  }
+  return variant === "title" ? (
+    <h1 className>{children}</h1>
+  ) : (
+    <h2 className>{children}</h2>
+  );
 }
 
 export default Heading;
